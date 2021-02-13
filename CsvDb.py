d3f96b91fc7451853eaee0660fe7d1e981efc984
danrayu <dan.rayu@gmail.com>
2021-02-13 13:30:03 +0200

    fixed load (1)

--- CsvDb.py
+++ CsvDb.py
@@ -7,14 +7,16 @@ def load():
         for row in csvfile:
             all_events.append(row)
     all_events.pop(0)
+    for event in range(len(all_events)):
+        all_events[event] = all_events[event].split(",")
     return all_events
 
 
 def get_event_type(event_type, all_events):
     selected_event_list = []
-    for event in range(len(all_events)):
-        all_events[event] = all_events[event].split(",")
     for event in all_events:
         if event[4] == event_type:
             selected_event_list.append(event)
     return selected_event_list
+
+
